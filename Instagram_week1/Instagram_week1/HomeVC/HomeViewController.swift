94745b36e6b0a45c11646deb127fda1c2f222e83
khnpoq@naver.com <khnpoq@naver.com>
2022-05-05 14:11:20 +0900

    [FEAT] 뷰 구현

---
+++ Instagram_week1/Instagram_week1/HomeVC/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  Instagram_week1
+//
+//  Created by 김하늘 on 2022/05/03.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
